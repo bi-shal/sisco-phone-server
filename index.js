f845c93bc3bfd3149630c51943217fff9d8d36ba
bi-shal <bishalsarkar336@gmail.com>
2022-11-25 12:03:08 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(express());
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://<username>:<password>@cluster0.eubulyg.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+
+
+
+//test
+app.get('/', async (req,res) => {
+    res.send('site running')
+})
+// jwt-
+
+
+app.listen((port), ()=> console.log(`site on runing ${port}`))
