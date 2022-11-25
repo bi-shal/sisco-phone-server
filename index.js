183fe60caae333a25d86592b4f0d49b7c0b923ab
bi-shal <bishalsarkar336@gmail.com>
2022-11-25 12:06:30 +0600

    mongodb

--- index.js
+++ index.js
@@ -13,11 +13,7 @@ app.use(express());
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://<username>:<password>@cluster0.eubulyg.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
 
 
 
